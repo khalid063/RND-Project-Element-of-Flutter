b0d17bacdcf381c3eaf0a4444a29d571786cf401
DeltaKhalid <khalid.delta063@gmail.com>
2023-11-13 17:57:03 +0600

    Third test commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'RND Project Element of Flutter',
+      title: 'RND Project Element of Flutter- This is Code Practice Project',
       home: HomeScreen(),
     );
   }
