09de5e8643e281ab006392900e9b34ec5dda8928
DeltaKhalid <khalid.delta063@gmail.com>
2024-01-08 16:55:08 +0600

    DropDown button work has been done

---
+++ lib/rnd_folder_01/dropdown_button/dropdown_button_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DropdownButtonScreen extends StatefulWidget {
+  const DropdownButtonScreen({super.key});
+
+  @override
+  State<DropdownButtonScreen> createState() => _DropdownButtonScreenState();
+}
+
+class _DropdownButtonScreenState extends State<DropdownButtonScreen> {
+
+  String areaSelectedValue = 'Area 1'; // Default selected value
+  List<String> areas = ['Area 1', 'Area 2', 'Area 3', 'Area 4'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dropdown Button'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //Text('Selected Value: $areaSelectedValue'),
+          SizedBox(height: 20),
+          DropdownButton<String>(
+            value: areaSelectedValue,
+            onChanged: (String? newValue) {
+              setState(() {
+                areaSelectedValue = newValue!;
+              });
+            },
+            items: areas.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
