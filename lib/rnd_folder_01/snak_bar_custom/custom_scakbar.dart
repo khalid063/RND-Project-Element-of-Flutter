09880caac6b4e20eac729de5c6f79792410c4faa
DeltaKhalid <khalid.delta063@gmail.com>
2024-01-22 17:13:15 +0600

    success custom snackbar design has been done

--- lib/rnd_folder_01/snak_bar_custom/custom_scakbar.dart
+++ lib/rnd_folder_01/snak_bar_custom/custom_scakbar.dart
@@ -11,7 +11,7 @@ class _CustomSnakbarState extends State<CustomSnakbar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
+      backgroundColor: Colors.white70,
       appBar: AppBar(
         title: Text('Custom Snakbar for Massage show'),
         centerTitle: true,
@@ -33,14 +33,30 @@ class _CustomSnakbarState extends State<CustomSnakbar> {
       SnackBar(
         content: Container(
           padding: EdgeInsets.all(16),
-          height: 80,
+          height: 70,
           decoration: BoxDecoration(
-            color: Colors.cyanAccent,
+            color: Colors.green,
             borderRadius: BorderRadius.all(Radius.circular(10),),
           ),
-          child: Row(
+          child: const Row(
             children: [
-
+              Icon(Icons.check_circle, color: Colors.white, size: 40,),
+              SizedBox(width: 20,),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Success', style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
+                    Spacer(),
+                    Text(
+                      'Flutter Custom Appbar Massage...',
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -50,5 +66,7 @@ class _CustomSnakbarState extends State<CustomSnakbar> {
       ),
     );
   }
+
+
 
 }
