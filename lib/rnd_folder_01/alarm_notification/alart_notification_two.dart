7131767fe587b3ff8441940394b770923f8c9137
DeltaKhalid <khalid.delta063@gmail.com>
2024-04-17 16:43:55 +0600

    Alarm Notification Design modification has been done

--- lib/rnd_folder_01/alarm_notification/alart_notification_two.dart
+++ lib/rnd_folder_01/alarm_notification/alart_notification_two.dart
@@ -5,6 +5,11 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+///--- packeg should be used into pubSpace.yaml---///
+// timezone: ^0.7.0
+// alarm: ^3.0.14 // not need into hare
+// audioplayers: ^1.2.0
+
 
 
 class AlarmNotificationScreenTwo extends StatefulWidget {
@@ -16,23 +21,17 @@ class AlarmNotificationScreenTwo extends StatefulWidget {
 
 class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo> {
 
-  //final audioPlayer = AudioPlayer();
-  String alarmSoundPath = 'assets/alarms/your_alarm_sound.mp3'; // Replace with your actual path
+  ///=============================== all variables =============================///
 
-  final DateTime now = DateTime.now();
-
-  // AudioCache audioCache = AudioCache();
-  // AudioPlayer audioPlayer = AudioPlayer();
-
-  final audioCache = AudioCache(); // For loading and caching
+  ///--- Object of AudioPlayer ---///
   final player = AudioPlayer(); // For playing
 
-  // final PlayAudio = AudioCache();
-
-
+  ///--- Today time ---///
   late String _timeString;
   TimeOfDay _timeOfDay = TimeOfDay(hour: 0, minute: 0);
 
+
+  ///=============================== initState =================================///
   @override
   void initState() {
     super.initState();
@@ -40,131 +39,9 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
     Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
   }
 
-  // void playAlarmSound() async {
-  //   await audioPlayer.play(assets/alarm_bell.mp3);
-  // }
-  // void playAlarmSound() async {
-  //   await audioCache.play('assets/alarm_bell.mp3'); // Use string literal for path
-  // }
+  ///=============================== Function Write Area =======================///
 
-
-  // void playAlarmSound() async {
-  //   // Construct the correct asset path
-  //   String alarmSoundPath = 'assets/alarm_bell.mp3'; // Replace with your actual filename
-  //
-  //   await audioPlayer.play(alarmSoundPath);
-  // }
-
-  // void playAlarmSound() {
-  //   // Construct the correct asset path
-  //   String alarmSoundPath = 'alarm_bell.mp3'; // Replace with your actual filename
-  //
-  //   audioCache.play(alarmSoundPath);
-  // }
-
-  // void playAlarmSound() {
-  //   // Construct the correct asset path
-  //   String alarmSoundPath = 'assets/alarm_bell.mp3'; // Replace with your actual filename
-  //
-  //   // Create an instance of UriAudioSource
-  //   Uri uri = Uri.parse(alarmSoundPath);
-  //   UriAudioSource uriAudioSource = UriAudioSource(uri: uri);
-  //
-  //   // Use play method of AudioPlayer
-  //   audioPlayer.play(uriAudioSource);
-  // }
-
-
-
-  void scheduleAlarm(DateTime alarmTime) async {
-    // Your logic to schedule the alarm (using a Timer, Alarm Service, etc.)
-    await Future.delayed(Duration(seconds: alarmTime.difference(DateTime.now()).inSeconds));
-    //playAlarmSound();
-  }
-
-
-
-
-
-  // void _getTime() {
-  //   final DateTime now = DateTime.now();
-  //   final String formattedTime = _formatDateTime(now);
-  //   setState(() {
-  //     _timeString = formattedTime;
-  //   });
-  //
-  //   // Check if the current time matches the picked time
-  //   if (_timeString == _timeOfDay.format(context)) {
-  //     _showAlarmPopup();
-  //     playAlarmSound();
-  //   }
-  // }
-
-  // void _getTime() {
-  //   print('getTime method is called');
-  //   final DateTime now = DateTime.now();
-  //   final formattedTime = _formatDateTime(now);
-  //   setState(() {
-  //     _timeString = formattedTime;
-  //   });
-  //
-  //   // Check if the current time (now) matches the picked time (_timeOfDay)
-  //   if (now.hour == _timeOfDay.hour && now.minute == _timeOfDay.minute) {
-  //     print('if condition getTime method is called');
-  //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Alarm is ringing')));
-  //     //_showAlarmPopup();
-  //     //playAlarmSound();
-  //   }
-  // }
-
-  // void _getTime() {
-  //   print('getTime method is called');
-  //   final DateTime now = DateTime.now();
-  //   final formattedTime = _formatDateTime(now);
-  //   setState(() {
-  //     _timeString = formattedTime;
-  //   });
-  //
-  //   // Check if the current time (now) matches the picked time (_timeOfDay)
-  //   if (now.hour == _timeOfDay.hour && now.minute == _timeOfDay.minute) {
-  //     print('massage 0091 :if condition getTime method is called');
-  //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Alarm is ringing')));
-  //     //_showAlarmPopup();
-  //     //playAlarmSound();
-  //   }
-  //   else if (now.hour != _timeOfDay.hour && now.minute != _timeOfDay.minute) {
-  //     print('massage 0092 : if condition getTime method is called ');
-  //     return;
-  //     //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Alarm is ringing')));
-  //     //_showAlarmPopup();
-  //     //playAlarmSound();
-  //   }
-  //
-  //
-  // }
-
-  // void _getTime() {
-  //   print('getTime method is called');
-  //   final DateTime now = DateTime.now();
-  //
-  //   // Check if the second is 01
-  //   if (now.second == 1) {
-  //     final formattedTime = _formatDateTime(now);
-  //     setState(() {
-  //       _timeString = formattedTime;
-  //     });
-  //
-  //     // Check if the current time (now) matches the picked time (_timeOfDay)
-  //     if (now.hour == _timeOfDay.hour && now.minute == _timeOfDay.minute) {
-  //       print('massage 0091 :if condition getTime method is called');
-  //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Alarm is ringing')));
-  //       //_showAlarmPopup();
-  //       //playAlarmSound();
-  //     }
-  //   }
-  //
-  // }
-
+  ///--- get time for pick time for Alarm ---///
   void _getTime() {
     final DateTime now = DateTime.now();
     final formattedTime = _formatDateTime(now);
@@ -182,9 +59,7 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
     }
   }
 
-
-
-
+  ///--- Formated Time for Alarm ---///
   String _formatDateTime(DateTime dateTime) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
     String period = (dateTime.hour < 12) ? 'AM' : 'PM';
@@ -194,6 +69,7 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
   }
 
 
+  ///--- Pick time for alarm ---///
   void _showTimePicker() {
     showTimePicker(
       context: context,
@@ -205,6 +81,7 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
     });
   }
 
+  ///--- popup for alarm off ---///
   void _showAlarmPopup() {
     showDialog(
       context: context,
@@ -231,6 +108,22 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
     );
   }
 
+  ///--- Player Sound Start Function ---///
+  Future<void> playSound() async {
+    //String audioPath = "audio/alart_sound_two.wav";
+    //String audioPath = "assets/audio/alart_sound_two.wav";
+    String audioPath = "audio/alart_sound_two.wav";
+    await player.play(AssetSource(audioPath));
+  }
+
+  ///--- Player Sound Off Function ---///
+  void stopAlarmSound() async {
+    if (player != null) {
+      await player.stop();
+    }
+  }
+
+  ///=============================== Scaffold Area =============================///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -244,7 +137,7 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
           Center(
             child: Text(
               _timeString,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 40,
                 color: Colors.pink,
                 fontWeight: FontWeight.w600,
@@ -257,30 +150,15 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
             child: Center(
               child: Text(
                 _timeOfDay.format(context),
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25,
                 ),
               ),
             ),
           ),
-          SizedBox(height: 50),
+          const SizedBox(height: 50),
 
-
-
-          // ElevatedButton(
-          //   onPressed: () async {
-          //     _showTimePicker();
-          //     _getTime();
-          //     final PlayerAudio = AudioCache();
-          //     await PlayerAudio.play("assets/images/alarm_bell.mp3");
-          //     if(mounted) {
-          //       setState(() {});
-          //     }
-          //   },
-          //   child: Text('Pick Time'),
-          // )
-
-
+          ///============================== Pick Time ==========================///
           ElevatedButton(
             onPressed: () async {
               // ...
@@ -292,52 +170,26 @@ class _AlarmNotificationScreenTwoState extends State<AlarmNotificationScreenTwo>
             },
             //child: SvgPicture.asset('assets/images/eye_hide_icon.svg'),
             //child: SvgPicture.asset('assets/images/eye_hide_icon.svg'),
-            child: Text('Pick Alarm Time'),
+            child: const Text('Pick Alarm Time'),
           ),
-          SizedBox(height: 30,),
+          const SizedBox(height: 30,),
+
+          ///============================= sound Start Button ==================///
           ElevatedButton(onPressed: (){
             playSound();
-          }, child: Text('Sound'))
+          }, child: const Text('Sound Start'),),
+          SizedBox(height: 30,),
+
+          ///============================= sound Off Button ==================///
+          ElevatedButton(onPressed: (){
+            stopAlarmSound();
+          }, child: const Text('Sound Off'),),
 
         ],
       ),
     );
   }
 
-  Future<void> playSound() async {
-    //String audioPath = "audio/alart_sound_two.wav";
-    //String audioPath = "assets/audio/alart_sound_two.wav";
-    String audioPath = "audio/alart_sound_two.wav";
-    await player.play(AssetSource(audioPath));
-  }
-
-  void stopAlarmSound() async {
-    if (player != null) {
-      await player.stop();
-    }
-  }
-
-  //
-  // void stopAlarmSound() async {
-  //   String audioPath = "audio/alart_sound_two.wav";
-  //   if (audioPlayer != null) {
-  //     await audioPlayer.stop();
-  //   }
-  //   // await player.stop(AssetSource(audioPath));
-  //   //await audioPlayer.stop();
-  // }
-
-
 }
 
-// class Sound {
-//   static final AudioPlayer player = AudioPlayer();
-//
-//   static void playSound(int option){
-//     player.audioCache.prefix = 'lib/assets/audio/';
-//     player.setPlayerMode(PlayerMode.mediaPlayer);
-//     player.play(AssetSource('note$option.wav'), mode: PlayerMode.lowLatency);
-//     player.audioCache.clearAll();
-//   }
-// }
 
